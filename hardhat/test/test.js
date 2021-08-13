05769df91fe1e2b2bf31d5ce57c1059b0ff5891b
adriandelgg <adriandelgadoc1@gmail.com>
2021-08-13 10:20:49 -0700

    Changed hh.config & added more dev dependencies

--- hardhat/test/test.js
+++ hardhat/test/test.js
@@ -1,13 +1,13 @@
 const { expect } = require('chai');
 
 describe('ContractName', function () {
-	let ContractName, owner, bob, contract, contract2;
+	let ContractFactory, owner, bob, contract, contract2;
 	const oneEther = ethers.BigNumber.from('1000000000000000000');
 
 	beforeEach(async function () {
-		ContractName = await ethers.getContractFactory('ContractName');
+		ContractFactory = await ethers.getContractFactory('ContractName');
 		[owner, bob] = await ethers.getSigners();
-		contract = await ContractName.deploy();
+		contract = await ContractFactory.deploy();
 		contract2 = contract.connect(bob);
 	});
 });
